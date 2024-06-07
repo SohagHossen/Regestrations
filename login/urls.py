6203586d1ebfd1ef96556d17d61a4ccc62529ec1
SohagHossen <sohaghossen.cse@gmail.com>
2024-06-08 01:57:21 +0600

    add login and registrations page

--- login/urls.py
+++ login/urls.py
@@ -5,4 +5,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.user_login, name='login'),
+    path('home/',views.home_page, name='home'),
+    path('register/', views.registrations, name='register'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,27 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 def user_login(request):
 
     return render(request,'templates/login.html')
+def home_page(request):
+    pass
+    return render(request,'templates/home.html')
+
+def registrations(request):
+
+    if request.method == "POST":
+        fname =request.POST.get('fristname')
+        lname =request.POST.get('lastname')
+        uname =request.POST.get('username')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        dob=request.POST.get('dob')
+        gender = request.POST.get('gender')
+        pass1 = request.POST.get('password')
+        pass2 = request.POST.get('repassword')
+        my_user =User.object.create_user(fname,lname,uname,email,phone,dob,gender,pass1)
+        my_user.save()
+        print(my_user)
+    return render(request,'templates/resistrations.html')
+
